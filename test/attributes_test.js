9cc0ea273961994e92a6b61fecd2c548720e1466
Mathieu Martin <webmat@gmail.com>
2012-04-13 07:53:33 -0400

    Add more tests for the most common attributes

--- test/attributes_test.js
+++ test/attributes_test.js
@@ -3,11 +3,15 @@
 /*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
 (function(el, document) {
 
-  test("creating element attributes", function() {
-    var e = el('p', {'data-alice': 'bob'});
+  test("creating an element with attributes", function() {
+    var e = el('p', {id: 'my-id', 'class': 'my-class', 'data-alice': 'bob'});
+
     var dataAttribute = e.attributes.getNamedItem('data-alice');
     ok(dataAttribute);
     equal(dataAttribute.value, 'bob');
+
+    equal(e.id, 'my-id');
+    equal(e.className, 'my-class');
   });
 
   test("setting an attribute to null removes it from the element", function() {
